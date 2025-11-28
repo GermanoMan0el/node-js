9169883212ff02de390196f1d91de66cc2e197ff
GermanoMan0el <germanomanoel007@gmail.com>
2025-11-28 20:11:42 -0300

    arquitetura de pastas para projetos node

---
+++ modulo-02/aula-08/src/dist/script.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let node = "Node.js";
+let name = "Germano Manoel";
+console.log(`Olá meu nome é ${name}, estou estudando a tecnologia ${node}!!`);
+//# sourceMappingURL=script.js.map
---
+++ modulo-02/aula-08/src/script.ts
@@ -0,0 +1,4 @@
+let node:string = "Node.js";
+let name:string = "Germano Manoel";
+
+console.log(`Olá meu nome é ${name}, estou estudando a tecnologia ${node}!!`);
