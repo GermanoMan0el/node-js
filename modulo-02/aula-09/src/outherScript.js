077544560868acb5c1fc060ae6ca2ce013b90e51
GermanoMan0el <germanomanoel007@gmail.com>
2025-11-28 21:02:14 -0300

    usando a funcionalidade --watch em um arquivo `.js` e `.ts`

---
+++ modulo-02/aula-09/src/outherScript.js
@@ -0,0 +1,6 @@
+// node --watch ./src/outherScript.js
+
+let myName = "Manoel"
+
+console.log(`Meu nome: ${myName}`);
+console.log("Amo estudar programação!!!");
---
+++ modulo-02/aula-09/src/script.ts
@@ -0,0 +1,6 @@
+// 1. Instalar a biblioteca tsx
+// 2. Executar o comando: node --import=tsx --watch src/script.ts
+
+let name:string = "Germano Manoel";
+
+console.log(`Olá meu nome é ${name}!!!`);
